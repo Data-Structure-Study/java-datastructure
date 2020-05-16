0e5c95b35ea8294866f5312d293a4883955ec5ef
Hamill210 <han95210@naver.com>
2020-05-16 16:26:34 +0900

    [hamill] feat : Main 클래스 생성, LinkedList 클래스 생성 후 inner 클래스로 Node 클래스 생성

--- hamill/src/main/java/com/codesquad/datastructurestudy/linkedlist/LinkedList.java
+++ hamill/src/main/java/com/codesquad/datastructurestudy/linkedlist/LinkedList.java
@@ -1,4 +1,25 @@
 package com.codesquad.datastructurestudy.linkedlist;
 
 public class LinkedList {
+    // 첫 번째 노드를 가리키는 필드
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node {
+        // 데이터가 저장될 필드
+        private Object data;
+        // 다음 노드를 가리키는 필드
+        private Node next;
+        public Node(Object input) {
+            this.data = input;
+            this.next = null;
+        }
+        // 노드의 내용을 쉽게 출력해서 확인 해볼 수 있는 기능
+        public String toString() {
+            return String.valueOf(this.data);
+        }
+    }
+    
+
 }
---
+++ hamill/src/main/java/com/codesquad/datastructurestudy/linkedlist/Main.java
@@ -0,0 +1,8 @@
+package com.codesquad.datastructurestudy.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList numbers = new LinkedList();
+    }
+}
