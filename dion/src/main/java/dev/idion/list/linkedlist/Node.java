fa725aaabc888d684e6f1cfe6d07b6abc7b0f914
IdionKim <ksundong@gmail.com>
2020-05-16 15:59:29 +0900

    feat: node 추가 기능 구현

--- dion/src/main/java/dev/idion/list/linkedlist/Node.java
+++ dion/src/main/java/dev/idion/list/linkedlist/Node.java
@@ -1,11 +1,36 @@
 package dev.idion.list.linkedlist;
 
+/**
+ * Singly Linked List 구현체
+ * @author dion
+ * @version 1.0.0
+ */
 public class Node {
+    private static Node head;
+
     private int data;
     private Node nextNode;
 
     public Node(int data) {
         this.data = data;
         this.nextNode = null;
+    }
+
+    /**
+     * Node를 맨 마지막 위치에 추가합니다.
+     * @param newNode 마지막 위치에 추가할 노드
+     * @since 2020-05-16
+     */
+    public void appendNode(Node newNode) {
+        if (Node.head == null) {
+            Node.head = newNode;
+        } else {
+            Node tail = head;
+            while (tail.nextNode != null) {
+                tail = tail.nextNode;
+            }
+
+            tail.nextNode = newNode;
+        }
     }
 }
