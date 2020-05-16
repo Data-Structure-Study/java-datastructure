c501dfb7d6f59c85a893619a38f613cedbea9526
IdionKim <ksundong@gmail.com>
2020-05-16 15:55:00 +0900

    feat: node 생성기능 구현

--- dion/src/main/java/dev/idion/list/linkedlist/Node.java
+++ dion/src/main/java/dev/idion/list/linkedlist/Node.java
@@ -3,4 +3,9 @@ package dev.idion.list.linkedlist;
 public class Node {
     private int data;
     private Node nextNode;
+
+    public Node(int data) {
+        this.data = data;
+        this.nextNode = null;
+    }
 }
