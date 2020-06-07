873cf8e2c587e56ae810591ecc3f99ce185f63d7
IdionKim <ksundong@gmail.com>
2020-06-07 19:02:30 +0900

    큐 자료구조 생성 테스트 코드 작성

--- dion/src/test/java/dev/idion/queue/QueueTest.java
+++ dion/src/test/java/dev/idion/queue/QueueTest.java
@@ -1,14 +1,25 @@
 package dev.idion.queue;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 class QueueTest {
 
     @Test
-    @DisplayName("Queue test")
-    void queueTest() {
-        Assertions.fail("Not Implemented");
+    @DisplayName("Queue creation test")
+    void queueCreationTest() {
+        Queue<Integer> queue = new Queue<>();
+        assertThat(queue).isNotNull();
+        assertThat(queue.isEmpty()).isTrue();
+        assertThat(queue.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Queue enqueue test")
+    void queueEnqueueTest() {
+        fail("Not Implemented");
     }
 }
