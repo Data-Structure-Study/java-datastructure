11cfee17b3bc8a3c56eb97872e990beb22703360
IdionKim <ksundong@gmail.com>
2020-06-07 18:55:10 +0900

    큐 자료구조 테스트 코드 생성
    그레이들 의존성 추가(Junit jupiter, assertJ)

---
+++ dion/src/test/java/dev/idion/queue/QueueTest.java
@@ -0,0 +1,14 @@
+package dev.idion.queue;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class QueueTest {
+
+    @Test
+    @DisplayName("Queue test")
+    void queueTest() {
+        Assertions.fail("Not Implemented");
+    }
+}
