149248b3cf6c82bbe3d46e236b436baeae993f6b
IdionKim <ksundong@gmail.com>
2020-06-20 17:18:47 +0900

    feat: BinaryTree의 insertNode 메소드 개선 및 테스트 코드 작성으로 검증

--- dion/src/main/java/dev/idion/tree/binarytree/BinaryTree.java
+++ dion/src/main/java/dev/idion/tree/binarytree/BinaryTree.java
@@ -5,30 +5,23 @@ public class BinaryTree<E extends Comparable<E>> {
     private Node<E> root;
 
     public Node<E> insertNode(Node<E> node, E data) {
+        if (root == null) {
+            root = new Node<>(data);
+            return root;
+        }
         if (node == null) {
             node = new Node<>(data);
             return node;
         }
-        if (node.data.compareTo(data) < 0) {
-            node.right = insertNode(node.right, data);
+        if (node.getData().compareTo(data) < 0) {
+            node.setRight(insertNode(node.getRight(), data));
         } else {
-            node.left = insertNode(node.left, data);
+            node.setLeft(insertNode(node.getLeft(), data));
         }
         return node;
     }
 
-    private static class Node<T extends Comparable<T>> {
-
-        private final T data;
-        private Node<T> left;
-        private Node<T> right;
-
-        public Node(T data) {
-            this.data = data;
-        }
-
-        public T getData() {
-            return this.data;
-        }
+    public Node<E> getRoot() {
+        return root;
     }
 }
---
+++ dion/src/test/java/dev/idion/tree/binarytree/BinaryTreeTest.java
@@ -0,0 +1,45 @@
+package dev.idion.tree.binarytree;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BinaryTreeTest {
+
+    @Test
+    @DisplayName("binaryTree root node insert test")
+    void binaryTreeRootNodeInsertTest() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        Node<Integer> root = binaryTree.insertNode(binaryTree.getRoot(), 1);
+
+        Assertions.assertThat(binaryTree.getRoot()).isEqualTo(root);
+    }
+
+    @Test
+    @DisplayName("root node insertions test")
+    void rootNodeInsertionsTest() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        Node<Integer> node1 = binaryTree.insertNode(null, 1);
+        Node<Integer> node2 = binaryTree.insertNode(node1, 6);
+        Node<Integer> node3 = binaryTree.insertNode(node2, 3);
+        Node<Integer> node4 = binaryTree.insertNode(node3, 7);
+        Node<Integer> node5 = binaryTree.insertNode(node4, 9);
+
+        // Root Node 검증
+        Assertions.assertThat(binaryTree.getRoot()).isEqualTo(node1);
+        Assertions.assertThat(binaryTree.getRoot().getLeft()).isNull();
+        Assertions.assertThat(binaryTree.getRoot().getRight().getData()).isEqualTo(6);
+
+        // Root Node Right Node 검증
+        Assertions.assertThat(binaryTree.getRoot().getRight().getLeft().getData()).isEqualTo(3);
+        Assertions.assertThat(binaryTree.getRoot().getRight().getLeft().getLeft()).isNull();
+        Assertions.assertThat(binaryTree.getRoot().getRight().getLeft().getRight()).isNull();
+
+        Assertions.assertThat(binaryTree.getRoot().getRight().getRight().getData()).isEqualTo(7);
+        Assertions.assertThat(binaryTree.getRoot().getRight().getRight().getLeft()).isNull();
+
+        Assertions.assertThat(binaryTree.getRoot().getRight().getRight().getRight().getData()).isEqualTo(9);
+        Assertions.assertThat(binaryTree.getRoot().getRight().getRight().getRight().getLeft()).isNull();
+        Assertions.assertThat(binaryTree.getRoot().getRight().getRight().getRight().getRight()).isNull();
+    }
+}
