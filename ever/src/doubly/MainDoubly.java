f2527f3c40bee146ee35178fb9057e7703c38370
Ever <hsik0225@gmail.com>
2020-07-10 15:37:07 +0900

    [Ever] feat: Doubly Linked List 구현

---
+++ ever/src/doubly/MainDoubly.java
@@ -0,0 +1,38 @@
+package doubly;
+
+public class MainDoubly {
+    public static void main(String[] args) {
+
+        DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<>();
+        doublyLinkedList.addFront("apple");
+        doublyLinkedList.addAfter("apple", "pear");
+        doublyLinkedList.addAfter("pear", "cherry");
+        doublyLinkedList.addBefore("cherry", "orange");
+
+        doublyLinkedList.remove("cherry");
+
+        doublyLinkedList.addAfter("orange", "grape");
+
+        doublyLinkedList.remove("apple");
+
+        doublyLinkedList.remove("pear");
+
+        doublyLinkedList.remove("orange");
+
+        doublyLinkedList.remove("grape");
+
+        /* 실행 결과
+
+        apple
+        apple pear
+        apple pear cherry
+        apple pear orange cherry
+        apple pear orange
+        apple pear orange grape 
+        pear orange grape
+        orange grape
+        grape
+
+         */
+    }
+}
