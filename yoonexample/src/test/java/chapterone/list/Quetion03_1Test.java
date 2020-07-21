e23b53f68c0da51b3c921f2be5b1f9e7cb4b5ce3
IdionKim <ksundong@gmail.com>
2020-07-21 13:47:51 +0900

    feat: 문제03-1 테스트 예제코드 생성

---
+++ yoonexample/src/test/java/chapterone/list/Quetion03_1Test.java
@@ -0,0 +1,43 @@
+package chapterone.list;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class Question03_1Test {
+
+  @Test
+  void 리스트_자료구조_활용() {
+    List<Integer> list = new ArrayList<>();
+
+    for (int i = 1; i < 10; i++) {
+      list.insert(i);
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+      assertThat(list.get(i)).isEqualTo(i + 1);
+    }
+
+    int actual = 0;
+    int expected = 45;
+
+    for (int i = 0; i < list.size(); i++) {
+      actual += list.get(i);
+    }
+
+    assertThat(actual).isEqualTo(expected);
+
+    for (int i = 0; i < list.size(); i++) {
+      int data = list.get(i);
+      if (data % 2 == 0 || data % 3 == 0) {
+        list.remove(i);
+      }
+    }
+
+    // 1, 5, 7
+    assertThat(list.size()).isEqualTo(3);
+    assertThat(list.get(0)).isEqualTo(1);
+    assertThat(list.get(0)).isEqualTo(5);
+    assertThat(list.get(0)).isEqualTo(7);
+  }
+}
