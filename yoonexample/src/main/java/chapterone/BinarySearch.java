c5f3f3848d647238beb6320678b60714604d66ad
IdionKim <ksundong@gmail.com>
2020-07-21 13:16:53 +0900

    refactor: C 스타일로 작성된 배열 수정

--- yoonexample/src/main/java/chapterone/BinarySearch.java
+++ yoonexample/src/main/java/chapterone/BinarySearch.java
@@ -2,7 +2,7 @@ package chapterone;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int arr[] = {1, 3, 5, 7, 9};
+        int[] arr = {1, 3, 5, 7, 9};
 
         int idx = bSearch(arr, 7);
         printSearchInfo(idx);
