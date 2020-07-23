82d4bfda165a4f4ec1c89e6a23319736a570c23a
IdionKim <ksundong@gmail.com>
2020-07-23 18:27:40 +0900

    feat: dummy 노드를 가진 링크드 리스트 정의

---
+++ yoonexample/src/main/java/list/DummyLinkedList.java
@@ -0,0 +1,55 @@
+package list;
+
+import java.util.Comparator;
+
+public class DummyLinkedList<E> implements SortedList<E> {
+
+  private int size;
+  private Node<E> head = new Node<>(null);
+  private Comparator<E> comp;
+
+  @Override
+  public void setSortRule(Comparator<E> comp) {
+    this.comp = comp;
+  }
+
+  @Override
+  public void insert(E data) {
+
+  }
+
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return false;
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    return false;
+  }
+
+  @Override
+  public E get(int index) {
+    return null;
+  }
+
+  @Override
+  public E remove(int index) {
+    return null;
+  }
+
+  private static class Node<T> {
+
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+      this.data = data;
+    }
+  }
+}
