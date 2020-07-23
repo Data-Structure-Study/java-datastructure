8f32da4643afe9aa5d1a70c4857494a08247b299
IdionKim <ksundong@gmail.com>
2020-07-23 23:31:08 +0900

    feat: 정렬 삽입 예제 코드 추가

--- yoonexample/src/main/java/list/DummyLinkedList.java
+++ yoonexample/src/main/java/list/DummyLinkedList.java
@@ -23,6 +23,7 @@ public class DummyLinkedList<E> implements SortedList<E> {
   }
 
   // 인터페이스를 통해 제공되는 메소드가 아님!
+  // head에 가까운 순서로 데이터 저장
   private void insertToHead(E data) {
     Node<E> newNode = new Node<>(data);
 
@@ -34,7 +35,19 @@ public class DummyLinkedList<E> implements SortedList<E> {
 
   // 인터페이스를 통해 제공되는 메소드가 아님!
   private void insertWithSortRule(E data) {
+    Node<E> newNode = new Node<>(data);
+    Node<E> prev = this.head;
 
+    // 두번째 조건의 의미: 입력된 데이터가 정렬 순서상 비교하는 데이터보다 뒤에 위치하는 경
+    // head -> 1 -> 2 -> 3 -> 4 일 때, 2가 들어오면 처음에 1과 비교, 1보다 크므로 다음 순서 2와 비교, 같으므로 해당 위치에 삽입.
+    while (prev.next != null && comp.compare(data, prev.next.data) > 0) {
+      prev = prev.next;
+    }
+
+    newNode.next = prev.next;
+    prev.next = newNode;
+
+    this.size++;
   }
 
   @Override
