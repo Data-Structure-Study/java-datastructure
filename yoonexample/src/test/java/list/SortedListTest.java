1ea78ac13d9fdb0183eb23acd8fbd80337c16675
IdionKim <ksundong@gmail.com>
2020-07-23 23:11:57 +0900

    test: 정렬 삽입 테스트 코드 추가

--- yoonexample/src/test/java/list/SortedListTest.java
+++ yoonexample/src/test/java/list/SortedListTest.java
@@ -57,4 +57,30 @@ class SortedListTest {
     assertThat(list.get(1)).isEqualTo(11);
     assertThat(list.get(2)).isEqualTo(11);
   }
+
+  @Test
+  void 정렬기준_설정_후_데이터_5개_저장_후_22인_데이터_모두_삭제() {
+    SortedList<Integer> list = new DummyLinkedList<>();
+
+    list.setSortRule((o1, o2) -> o2 - o1); // 내림차
+
+    list.insert(11);
+    list.insert(22);
+    list.insert(11);
+    list.insert(22);
+    list.insert(33);
+
+    // Iterable을 구현하면 enhanced for loop를 사용할 수 있습니다.
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i) == 22) {
+        list.remove(i);
+        i--; // 삭제되면 index가 조정되므로 해당 index부터 다시 확인해야 합니다.
+      }
+    }
+
+    assertThat(list.size()).isEqualTo(3);
+    assertThat(list.get(0)).isEqualTo(33);
+    assertThat(list.get(1)).isEqualTo(11);
+    assertThat(list.get(2)).isEqualTo(11);
+  }
 }
