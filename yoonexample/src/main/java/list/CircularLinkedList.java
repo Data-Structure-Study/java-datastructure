10f856935289035271195d147d351217ed9209a0
IdionKim <ksundong@gmail.com>
2020-07-27 20:08:36 +0900

    feat: insert 부분 예제코드 구현

--- yoonexample/src/main/java/list/CircularLinkedList.java
+++ yoonexample/src/main/java/list/CircularLinkedList.java
@@ -6,8 +6,17 @@ public class CircularLinkedList<E> implements List<E> {
   private int size;
 
   @Override
-  public void insert(E data) {
-
+  public void insert(E data) { // 의도는 tail 위치에 새로운 노드를 추가하는 것입니다.
+    Node<E> newNode = new Node<>(data);
+    if (tail == null) {
+      tail = newNode;
+      newNode.next = newNode;
+    } else {
+      newNode.next = tail.next; // 새로 추가될 노드의 다음 노드가 기존의 첫번째 노드여야 합니다.
+      tail.next = newNode; // 마지막 노드의 다음 노드가 새로 추가될 노드여야 합니다.
+      tail = newNode; // 마지막 노드는 새로 추가된 노드여야 합니다.
+    }
+    size++;
   }
 
   @Override
