5d1e20f1aa08e1f9f3207f863b6303ed3c974bd0
IdionKim <ksundong@gmail.com>
2020-07-28 11:04:39 +0900

    feat: DummyDoublyLinkedList 데이터 삽입 예제 코드 추가

--- yoonexample/src/main/java/list/DummyDoublyLinkedList.java
+++ yoonexample/src/main/java/list/DummyDoublyLinkedList.java
@@ -15,7 +15,14 @@ public class DummyDoublyLinkedList<E> implements List<E> {
 
   @Override
   public void insert(E data) { // tail쪽으로 데이터가 추가 됩니다.
+    Node<E> newNode = new Node<>(data);
 
+    newNode.prev = this.tail.prev; // 새로운 노드의 이전 노드는 마지막 노드입니다.
+    newNode.next = this.tail; // 새로운 노드의 다음 노드는 tail입니다.
+    this.tail.prev.next = newNode; // 이전 마지막 노드의 다음을 새로운 노드로 지정합니다.
+    this.tail.prev = newNode; // tail 노드의 이전노드를 새로운 노드로 지정합니다.
+
+    this.size++;
   }
 
   @Override
