a27759d5b4beecd13559d919621b630c287b9544
IdionKim <ksundong@gmail.com>
2020-07-30 12:18:35 +0900

    feat: List 기반의 스택 초기화 예제 코드 작성

---
+++ yoonexample/src/main/java/stack/ListStack.java
@@ -0,0 +1,42 @@
+package stack;
+
+public class ListStack<E> implements Stack<E> {
+
+  private int size;
+  private Node<E> head;
+
+  @Override
+  public int size() {
+    return this.size;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  @Override
+  public void push(E data) {
+
+  }
+
+  @Override
+  public E pop() {
+    return null;
+  }
+
+  @Override
+  public E peek() {
+    return null;
+  }
+
+  private static class Node<T> {
+
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+      this.data = data;
+    }
+  }
+}
