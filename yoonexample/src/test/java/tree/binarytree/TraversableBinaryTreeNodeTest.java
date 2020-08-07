2bcb8302cb039dcd65503eb8478d94294d9288ef
IdionKim <ksundong@gmail.com>
2020-08-07 10:41:32 +0900

    test: 순회 가능한 이진 트리 테스트 코드 작성

---
+++ yoonexample/src/test/java/tree/binarytree/TraversableBinaryTreeNodeTest.java
@@ -0,0 +1,103 @@
+package tree.binarytree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TraversableBinaryTreeNodeTest {
+
+  @Test
+  @DisplayName("이진_트리_생성_및_초기화_테스트")
+  void 이진_트리_생성_및_초기화_테스트() {
+    TraversableBinaryTreeNode<Integer> bt = new TraversableBinaryTreeNodeImpl<>(1);
+
+    assertThat(bt).isNotNull();
+    assertThat(bt.getData()).isEqualTo(1);
+    assertThat(bt.getLeftSubTree()).isNull();
+    assertThat(bt.getRightSubTree()).isNull();
+  }
+
+  @Test
+  @DisplayName("이진_트리_저장_및_출력_테스트")
+  void 이진_트리_저장_및_출력_테스트() {
+    TraversableBinaryTreeNode<Integer> bt1 = new TraversableBinaryTreeNodeImpl<>(1);
+    TraversableBinaryTreeNode<Integer> bt2 = new TraversableBinaryTreeNodeImpl<>(2);
+    TraversableBinaryTreeNode<Integer> bt3 = new TraversableBinaryTreeNodeImpl<>(3);
+    TraversableBinaryTreeNode<Integer> bt4 = new TraversableBinaryTreeNodeImpl<>(4);
+
+    bt1.setLeftSubTree(bt2);
+    bt1.setRightSubTree(bt3);
+    bt2.setLeftSubTree(bt4);
+
+    assertThat(bt1).isNotNull();
+    assertThat(bt2).isNotNull();
+    assertThat(bt3).isNotNull();
+    assertThat(bt4).isNotNull();
+    assertThat(bt1.getData()).isEqualTo(1);
+    assertThat(bt2.getData()).isEqualTo(2);
+    assertThat(bt3.getData()).isEqualTo(3);
+    assertThat(bt4.getData()).isEqualTo(4);
+    assertThat(bt1.getLeftSubTree().getData()).isEqualTo(2);
+    assertThat(bt1.getRightSubTree().getData()).isEqualTo(3);
+    assertThat(bt1.getLeftSubTree().getLeftSubTree().getData()).isEqualTo(4);
+    assertThat(bt1.getLeftSubTree().getRightSubTree()).isNull();
+  }
+
+  @Test
+  @DisplayName("이진_트리_전위_순회_테스트")
+  void 이진_트리_전위_순회_테스트() {
+    TraversableBinaryTreeNode<Integer> bt1 = new TraversableBinaryTreeNodeImpl<>(1);
+    TraversableBinaryTreeNode<Integer> bt2 = new TraversableBinaryTreeNodeImpl<>(2);
+    TraversableBinaryTreeNode<Integer> bt3 = new TraversableBinaryTreeNodeImpl<>(3);
+    TraversableBinaryTreeNode<Integer> bt4 = new TraversableBinaryTreeNodeImpl<>(4);
+
+    bt1.setLeftSubTree(bt2);
+    bt1.setRightSubTree(bt3);
+    bt2.setLeftSubTree(bt4);
+
+    String expected = "1 2 4 3";
+    StringBuilder sb = new StringBuilder();
+    bt1.preorderTraverse(sb);
+
+    assertThat(sb.toString()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("이진_트리_중위_순회_테스트")
+  void 이진_트리_중위_순회_테스트() {
+    TraversableBinaryTreeNode<Integer> bt1 = new TraversableBinaryTreeNodeImpl<>(1);
+    TraversableBinaryTreeNode<Integer> bt2 = new TraversableBinaryTreeNodeImpl<>(2);
+    TraversableBinaryTreeNode<Integer> bt3 = new TraversableBinaryTreeNodeImpl<>(3);
+    TraversableBinaryTreeNode<Integer> bt4 = new TraversableBinaryTreeNodeImpl<>(4);
+
+    bt1.setLeftSubTree(bt2);
+    bt1.setRightSubTree(bt3);
+    bt2.setLeftSubTree(bt4);
+
+    String expected = "4 2 1 3";
+    StringBuilder sb = new StringBuilder();
+    bt1.inorderTraverse(sb);
+
+    assertThat(sb.toString()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("이진_트리_후위_순회_테스트")
+  void 이진_트리_후위_순회_테스트() {
+    TraversableBinaryTreeNode<Integer> bt1 = new TraversableBinaryTreeNodeImpl<>(1);
+    TraversableBinaryTreeNode<Integer> bt2 = new TraversableBinaryTreeNodeImpl<>(2);
+    TraversableBinaryTreeNode<Integer> bt3 = new TraversableBinaryTreeNodeImpl<>(3);
+    TraversableBinaryTreeNode<Integer> bt4 = new TraversableBinaryTreeNodeImpl<>(4);
+
+    bt1.setLeftSubTree(bt2);
+    bt1.setRightSubTree(bt3);
+    bt2.setLeftSubTree(bt4);
+
+    String expected = "4 2 3 1";
+    StringBuilder sb = new StringBuilder();
+    bt1.postorderTraverse(sb);
+
+    assertThat(sb.toString()).isEqualTo(expected);
+  }
+}
