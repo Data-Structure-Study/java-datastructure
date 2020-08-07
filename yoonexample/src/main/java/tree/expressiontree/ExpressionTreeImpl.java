7b56bcdd3b8c73f32043d6f308a0fad80ac3b6b4
IdionKim <ksundong@gmail.com>
2020-08-07 13:54:21 +0900

    feat: 수식 트리 초기화 코드 구현

---
+++ yoonexample/src/main/java/tree/expressiontree/ExpressionTreeImpl.java
@@ -0,0 +1,47 @@
+package tree.expressiontree;
+
+import stack.ListStack;
+import stack.Stack;
+import tree.binarytree.TraversableBinaryTreeNode;
+import tree.binarytree.TraversableBinaryTreeNodeImpl;
+
+public class ExpressionTreeImpl implements ExpressionTree {
+
+  private TraversableBinaryTreeNode<Character> root;
+
+  public ExpressionTreeImpl(char[] postfixExpression) {
+    Stack<TraversableBinaryTreeNode<Character>> stack = new ListStack<>();
+
+    for (char expr : postfixExpression) {
+      TraversableBinaryTreeNode<Character> node = new TraversableBinaryTreeNodeImpl<>(expr);
+      if (!Character.isDigit(expr)) { // 숫자가 아니라면, 연산자임
+        node.setRightSubTree(stack.pop());
+        node.setLeftSubTree(stack.pop());
+      }
+
+      stack.push(node);
+    }
+
+    root = stack.pop();
+  }
+
+  @Override
+  public int evaluateTree() {
+    return 0;
+  }
+
+  @Override
+  public void prefixTypeExpression(StringBuilder sb) {
+
+  }
+
+  @Override
+  public void infixTypeExpression(StringBuilder sb) {
+
+  }
+
+  @Override
+  public void postfixTypeExpression(StringBuilder sb) {
+
+  }
+}
