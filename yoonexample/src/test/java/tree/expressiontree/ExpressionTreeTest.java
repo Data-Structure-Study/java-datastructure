05d3a63eaccd462281c64c097dcba7e8fe2a02d3
IdionKim <ksundong@gmail.com>
2020-08-07 13:32:03 +0900

    test: 수식 트리 테스트 코드 작성

---
+++ yoonexample/src/test/java/tree/expressiontree/ExpressionTreeTest.java
@@ -0,0 +1,62 @@
+package tree.expressiontree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import stack.InfixToPostfix;
+
+class ExpressionTreeTest {
+
+  InfixToPostfix infixToPostfix;
+
+  @BeforeEach
+  void setUp() {
+    infixToPostfix = new InfixToPostfix();
+  }
+
+  @Test
+  @DisplayName("수식_트리_테스트")
+  void 수식_트리_테스트() {
+    String infixExpression = "1 + 2";
+    char[] postfixExpression = infixToPostfix.convertInputToPostfix(infixExpression);
+
+    ExpressionTree expressionTree = new ExpressionTreeImpl(postfixExpression);
+    assertThat(expressionTree.evaluateTree()).isEqualTo(3);
+
+    StringBuilder sb = new StringBuilder();
+    expressionTree.prefixTypeExpression(sb);
+    assertThat(sb.toString()).isEqualTo("+12");
+
+    sb = new StringBuilder();
+    expressionTree.infixTypeExpression(sb);
+    assertThat(sb.toString()).isEqualTo("1+2");
+
+    sb = new StringBuilder();
+    expressionTree.postfixTypeExpression(sb);
+    assertThat(sb.toString()).isEqualTo("12+");
+  }
+
+  @Test
+  @DisplayName("수식_트리_테스트2")
+  void 수식_트리_테스트2() {
+    String infixExpression = "1 + 2 * 7";
+    char[] postfixExpression = infixToPostfix.convertInputToPostfix(infixExpression);
+
+    ExpressionTree expressionTree = new ExpressionTreeImpl(postfixExpression);
+    assertThat(expressionTree.evaluateTree()).isEqualTo(3);
+
+    StringBuilder sb = new StringBuilder();
+    expressionTree.prefixTypeExpression(sb);
+    assertThat(sb.toString()).isEqualTo("*+127");
+
+    sb = new StringBuilder();
+    expressionTree.infixTypeExpression(sb);
+    assertThat(sb.toString()).isEqualTo("1+2*7");
+
+    sb = new StringBuilder();
+    expressionTree.postfixTypeExpression(sb);
+    assertThat(sb.toString()).isEqualTo("12+7*");
+  }
+}
