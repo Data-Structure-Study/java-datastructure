8336a9b7002684db97cd4812bcd08a7108762df6
IdionKim <ksundong@gmail.com>
2020-08-07 14:30:19 +0900

    feat: 수식 트리 계산 기능 추가

--- yoonexample/src/main/java/tree/expressiontree/ExpressionTreeImpl.java
+++ yoonexample/src/main/java/tree/expressiontree/ExpressionTreeImpl.java
@@ -27,6 +27,40 @@ public class ExpressionTreeImpl implements ExpressionTree {
 
   @Override
   public int evaluateTree() {
+    return evaluateTree(this.root);
+  }
+
+  private int evaluateTree(TraversableBinaryTreeNode<Character> node) {
+    TraversableBinaryTreeNode<Character> leftSubTree = node.getLeftSubTree();
+    TraversableBinaryTreeNode<Character> rightSubTree = node.getRightSubTree();
+
+    if (leftSubTree == null && rightSubTree == null) {
+      return Character.getNumericValue(node.getData()); // 단말 노드는 피연산자임
+    }
+
+    int op1 = 0;
+    int op2 = 0;
+    if (leftSubTree != null) {
+      op1 = evaluateTree(leftSubTree);
+    }
+    if (rightSubTree != null) {
+      op2 = evaluateTree(rightSubTree);
+    }
+
+    switch (node.getData()) {
+      case '+':
+        return op1 + op2;
+      case '-':
+        return op1 - op2;
+      case '*':
+        return op1 * op2;
+      case '/':
+        if (op2 == 0) {
+          throw new ArithmeticException("Can't be division by zero.");
+        }
+        return op1 / op2;
+    }
+
     return 0;
   }
 
