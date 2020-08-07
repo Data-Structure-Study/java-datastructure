30dd6d86700bf78985de0e2611c8a5b5c8c443a6
IdionKim <ksundong@gmail.com>
2020-08-07 11:09:51 +0900

    refactor: 설계상 문제로 인터페이스 변경

--- yoonexample/src/main/java/tree/binarytree/TraversableBinaryTreeNode.java
+++ yoonexample/src/main/java/tree/binarytree/TraversableBinaryTreeNode.java
@@ -5,7 +5,42 @@ package tree.binarytree;
  *
  * @param <E> 파라미터 타입
  */
-public interface TraversableBinaryTreeNode<E> extends BinaryTreeNode<E> {
+public interface TraversableBinaryTreeNode<E> {
+
+  /**
+   * 해당 노드에 저장된 데이터를 반환합니다.
+   *
+   * @return 저장된 데이터
+   */
+  E getData();
+
+  /**
+   * 해당 노드의 왼쪽 서브트리를 반환합니다.
+   *
+   * @return 왼쪽 서브트리
+   */
+  TraversableBinaryTreeNode<E> getLeftSubTree();
+
+  /**
+   * 왼쪽 서브트리를 등록합니다.
+   *
+   * @param subTree 대상이 될 노드
+   */
+  void setLeftSubTree(TraversableBinaryTreeNode<E> subTree);
+
+  /**
+   * 해당 노드의 오른쪽 서브트리를 반환합니다.
+   *
+   * @return 오른쪽 서브트리
+   */
+  TraversableBinaryTreeNode<E> getRightSubTree();
+
+  /**
+   * 오른쪽 서브트리를 등록합니다.
+   *
+   * @param subTree 대상이 될 노드
+   */
+  void setRightSubTree(TraversableBinaryTreeNode<E> subTree);
 
   /**
    * 전위 순회한 정보를 StringBuilder에 저장합니다.
