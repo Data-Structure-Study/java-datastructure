e6180763d087379cba60d80d81af16e351af194e
IdionKim <ksundong@gmail.com>
2020-08-12 18:42:16 +0900

    feat: Comparator를 이용하는 쓸만한 Heap 자료구조 인터페이스 정의

---
+++ yoonexample/src/main/java/heap/UsefulHeap.java
@@ -0,0 +1,30 @@
+package heap;
+
+/**
+ * Comparator를 이용한 쓸만한 Heap 자료구조
+ *
+ * @param <E> 타입 파라미터
+ */
+public interface UsefulHeap<E> {
+
+  /**
+   * 해당 Heap이 비어있는지 여부를 반환합니다.
+   *
+   * @return 해당 Heap이 비어있으면 true, 아니라면 false
+   */
+  boolean isEmpty();
+
+  /**
+   * 해당 Heap에 데이터를 저장합니다. 우선순위는 Comparator로 결정합니다.
+   *
+   * @param data 저장할 데이터
+   */
+  void insert(E data);
+
+  /**
+   * 해당 Heap의 최우선순위 데이터를 제거합니다.
+   *
+   * @return 힙에 저장되어 있던 최우선순위 데이터
+   */
+  E delete();
+}
