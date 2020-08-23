93209d0df1c6f5976033754d257a71c84209d88c
IdionKim <ksundong@gmail.com>
2020-08-23 20:05:48 +0900

    feat: 리밸런싱에 필요한 도구 정의 균형을 이루고 있는지 판단하는 메서드 작성

--- yoonexample/src/main/java/search/binarysearchtree/AVLTreeNode.java
+++ yoonexample/src/main/java/search/binarysearchtree/AVLTreeNode.java
@@ -1,10 +1,32 @@
 package search.binarysearchtree;
 
 import java.util.Comparator;
+import tree.binarytree.BinaryTreeNode;
 
 public class AVLTreeNode<E> extends LinkedBinarySearchTreeNode<E> {
 
   public AVLTreeNode(E data, Comparator<E> comparator) {
     super(data, comparator);
+  }
+
+  private int getHeight(BinaryTreeNode<E> binaryTreeNode) {
+    if (binaryTreeNode == null) {
+      return 0;
+    }
+    int leftHeight = getHeight(binaryTreeNode.getLeftSubTree());
+    int rightHeight = getHeight(binaryTreeNode.getRightSubTree());
+
+    return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1; // 큰 쪽의 높이 반환
+  }
+
+  private int getEquilibriumFactor(BinaryTreeNode<E> binaryTreeNode) {
+    if (binaryTreeNode == null) {
+      return 0;
+    }
+
+    int leftHeight = getHeight(binaryTreeNode.getLeftSubTree());
+    int rightHeight = getHeight(binaryTreeNode.getRightSubTree());
+
+    return leftHeight - rightHeight;
   }
 }
