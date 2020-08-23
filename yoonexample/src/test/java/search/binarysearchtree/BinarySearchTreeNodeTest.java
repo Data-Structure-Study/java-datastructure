dfc1c90ed263ef2d208fe0f5942d6d1b298920d1
IdionKim <ksundong@gmail.com>
2020-08-23 18:39:11 +0900

    test: AVL 트리 테스트 추가

--- yoonexample/src/test/java/search/binarysearchtree/BinarySearchTreeNodeTest.java
+++ yoonexample/src/test/java/search/binarysearchtree/BinarySearchTreeNodeTest.java
@@ -104,4 +104,106 @@ class BinarySearchTreeNodeTest {
     assertThat(bstRoot.search(8).getData())
         .isEqualTo(bstRoot.remove(8).getData());
   }
+
+  @Test
+  @DisplayName("AVL_트리_테스트")
+  void AVL_트리_테스트() {
+    BinarySearchTreeNode<Integer> bstRoot = new AVLTreeNode<>(1, (Comparator.comparingInt(o -> o)));
+    BinaryTreeNode<Integer> searchNode;
+
+    bstRoot.insert(5);
+    bstRoot.insert(6);
+    bstRoot.insert(2);
+    bstRoot.insert(8);
+    bstRoot.insert(3);
+    bstRoot.insert(9);
+
+    searchNode = bstRoot.search(1);
+    assertThat(searchNode).isNotNull();
+    assertThat(searchNode.getData()).isEqualTo(1);
+
+    searchNode = bstRoot.search(4);
+    assertThat(searchNode).isNull();
+
+    searchNode = bstRoot.search(6);
+    assertThat(searchNode).isNotNull();
+    assertThat(searchNode.getData()).isEqualTo(6);
+
+    searchNode = bstRoot.search(7);
+    assertThat(searchNode).isNull();
+
+    BinaryTreeNode<Integer> rootNode = bstRoot.search(5);
+    BinaryTreeNode<Integer> rootNodeLeftSubTree = rootNode.getLeftSubTree();
+    BinaryTreeNode<Integer> rootNodeRightSubTree = rootNode.getRightSubTree();
+
+    assertThat(rootNode.getData()).isEqualTo(5);
+    assertThat(rootNodeLeftSubTree.getData()).isEqualTo(2);
+    assertThat(rootNodeRightSubTree.getData()).isEqualTo(8);
+    assertThat(rootNodeLeftSubTree.getLeftSubTree().getData()).isEqualTo(1);
+    assertThat(rootNodeLeftSubTree.getRightSubTree().getData()).isEqualTo(3);
+    assertThat(rootNodeRightSubTree.getLeftSubTree().getData()).isEqualTo(6);
+    assertThat(rootNodeRightSubTree.getRightSubTree().getData()).isEqualTo(9);
+  }
+
+  @Test
+  @DisplayName("AVL_트리_제거_테스트")
+  void AVL_트리_제거_테스트() {
+    BinarySearchTreeNode<Integer> bstRoot = new AVLTreeNode<>(5, (Comparator.comparingInt(o -> o)));
+
+    bstRoot.insert(1);
+    bstRoot.insert(2);
+    bstRoot.insert(3);
+    bstRoot.insert(7);
+    bstRoot.insert(8);
+    bstRoot.insert(9);
+
+    BinaryTreeNode<Integer> rootNode = bstRoot.search(2);
+    BinaryTreeNode<Integer> rootNodeLeftSubTree = rootNode.getLeftSubTree();
+    BinaryTreeNode<Integer> rootNodeRightSubTree = rootNode.getRightSubTree();
+    BinaryTreeNode<Integer> rootNodeRightSubTreeLeftSubTree = rootNodeRightSubTree.getLeftSubTree();
+
+    assertThat(rootNode.getData()).isEqualTo(2);
+    assertThat(rootNodeLeftSubTree.getData()).isEqualTo(1);
+    assertThat(rootNodeRightSubTree.getData()).isEqualTo(8);
+    assertThat(rootNodeRightSubTreeLeftSubTree.getData()).isEqualTo(5);
+    assertThat(rootNodeRightSubTree.getRightSubTree().getData()).isEqualTo(9);
+    assertThat(rootNodeRightSubTreeLeftSubTree.getLeftSubTree().getData()).isEqualTo(3);
+    assertThat(rootNodeRightSubTreeLeftSubTree.getRightSubTree().getData()).isEqualTo(7);
+
+    assertThat(bstRoot.search(2).getData()).isEqualTo(bstRoot.remove(2).getData());
+    assertThat(bstRoot.search(7).getData()).isEqualTo(bstRoot.remove(7).getData());
+    assertThat(bstRoot.search(8).getData()).isEqualTo(bstRoot.remove(8).getData());
+    assertThat(bstRoot.search(5).getData()).isEqualTo(bstRoot.remove(5).getData());
+  }
+
+  @Test
+  @DisplayName("AVL_트리_제거_테스트2")
+  void AVL_트리_제거_테스트2() {
+    BinarySearchTreeNode<Integer> bstRoot = new AVLTreeNode<>(1, (Comparator.comparingInt(o -> o)));
+
+    bstRoot.insert(3);
+    bstRoot.insert(4);
+    bstRoot.insert(5);
+    bstRoot.insert(7);
+    bstRoot.insert(8);
+    bstRoot.insert(9);
+
+    BinaryTreeNode<Integer> rootNode = bstRoot.search(3);
+    BinaryTreeNode<Integer> rootNodeLeftSubTree = rootNode.getLeftSubTree();
+    BinaryTreeNode<Integer> rootNodeRightSubTree = rootNode.getRightSubTree();
+
+    assertThat(rootNode.getData()).isEqualTo(3);
+    assertThat(rootNodeLeftSubTree.getData()).isEqualTo(1);
+    assertThat(rootNodeRightSubTree.getData()).isEqualTo(7);
+    assertThat(rootNodeRightSubTree.getLeftSubTree().getData()).isEqualTo(5);
+    assertThat(rootNodeRightSubTree.getRightSubTree().getData()).isEqualTo(8);
+
+    assertThat(bstRoot.search(1).getData()).isEqualTo(bstRoot.remove(1).getData());
+    assertThat(bstRoot.search(3).getData()).isEqualTo(bstRoot.remove(3).getData());
+    assertThat(bstRoot.search(5).getData()).isEqualTo(bstRoot.remove(5).getData());
+    assertThat(bstRoot.search(4).getData()).isEqualTo(bstRoot.remove(4).getData());
+    assertThat(bstRoot.search(7).getData()).isEqualTo(bstRoot.remove(7).getData());
+    assertThat(bstRoot.search(9).getData()).isEqualTo(bstRoot.remove(9).getData());
+    assertThat(bstRoot.search(8).getData()).isEqualTo(bstRoot.remove(8).getData());
+  }
 }
