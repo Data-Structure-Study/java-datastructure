3a0495c09ab8c9c646fcb33057baf135f80473e0
IdionKim <ksundong@gmail.com>
2020-09-01 16:07:34 +0900

    test: bfs 테스트 코드 작성

--- yoonexample/src/test/java/graph/GraphTest.java
+++ yoonexample/src/test/java/graph/GraphTest.java
@@ -42,6 +42,27 @@ class GraphTest {
 
   @Test
   void dfsTest() {
+    setVertices();
+
+    assertThat(graph.depthFirstSearch(Point.A)).isEqualTo("A E D C B");
+    assertThat(graph.depthFirstSearch(Point.B)).isEqualTo("B E D C A");
+    assertThat(graph.depthFirstSearch(Point.C)).isEqualTo("C D E B A");
+    assertThat(graph.depthFirstSearch(Point.D)).isEqualTo("D E B A C");
+    assertThat(graph.depthFirstSearch(Point.E)).isEqualTo("E D C A B");
+  }
+
+  @Test
+  void bfsTest() {
+    setVertices();
+
+    assertThat(graph.breadthFirstSearch(Point.A)).isEqualTo("A E B D C");
+    assertThat(graph.breadthFirstSearch(Point.B)).isEqualTo("B E D A C");
+    assertThat(graph.breadthFirstSearch(Point.C)).isEqualTo("C D A E B");
+    assertThat(graph.breadthFirstSearch(Point.D)).isEqualTo("D E C B A");
+    assertThat(graph.breadthFirstSearch(Point.E)).isEqualTo("E D B A C");
+  }
+
+  private void setVertices() {
     graph.addEdge(Point.A, Point.B);
     graph.addEdge(Point.A, Point.C);
     graph.addEdge(Point.A, Point.E);
@@ -49,12 +70,6 @@ class GraphTest {
     graph.addEdge(Point.B, Point.E);
     graph.addEdge(Point.C, Point.D);
     graph.addEdge(Point.D, Point.E);
-
-    assertThat(graph.depthFirstSearch(Point.A)).isEqualTo("A E D C B");
-    assertThat(graph.depthFirstSearch(Point.B)).isEqualTo("B E D C A");
-    assertThat(graph.depthFirstSearch(Point.C)).isEqualTo("C D E B A");
-    assertThat(graph.depthFirstSearch(Point.D)).isEqualTo("D E B A C");
-    assertThat(graph.depthFirstSearch(Point.E)).isEqualTo("E D C A B");
   }
 
   private enum Point {
