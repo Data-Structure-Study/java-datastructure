d4a4c9d7e343d7e772f47f0f397a11ad64a6d77c
IdionKim <ksundong@gmail.com>
2020-09-02 13:03:41 +0900

    feat: 기존과 같은 부분 구현
    아... 그냥 상속을 통해서 확장해서 쓸 걸... 이라는 생각이 들었습니다.

--- yoonexample/src/main/java/graph/ListWeightGraph.java
+++ yoonexample/src/main/java/graph/ListWeightGraph.java
@@ -1,10 +1,15 @@
 package graph;
 
 import java.util.Comparator;
+import java.util.StringJoiner;
 import list.DummyDoublyLinkedList;
 import list.List;
 import priorityqueue.HeapPriorityQueue;
 import priorityqueue.PriorityQueue;
+import queue.LinkedListQueue;
+import queue.Queue;
+import stack.ListStack;
+import stack.Stack;
 
 public class ListWeightGraph implements WeightedGraph {
 
@@ -49,17 +54,83 @@ public class ListWeightGraph implements WeightedGraph {
 
   @Override
   public String showGraphEdgeInfo() {
-    return null;
+    StringBuilder sb = new StringBuilder();
+
+    for (List<Enum<?>> vertex : vertices) {
+      if (vertex.size() > 1) {
+        for (int i = 0; i < vertex.size(); i++) {
+          sb.append(vertex.get(i));
+          if (i == 0) {
+            sb.append(": ");
+          } else if (i < vertex.size() - 1) {
+            sb.append(" ");
+          }
+        }
+        sb.append("\n");
+      }
+    }
+
+    return sb.toString();
   }
 
   @Override
   public String depthFirstSearch(Enum<?> startV) {
-    return null;
+    boolean[] visited = new boolean[vertices.length];
+    StringJoiner sj = new StringJoiner(" ");
+    Stack<Enum<?>> vertexStack = new ListStack<>();
+    vertexStack.push(startV);
+
+    while (!vertexStack.isEmpty()) {
+      Enum<?> visitV = vertexStack.pop();
+
+      if (visitVertex(visited, visitV)) {
+        sj.add(visitV.toString());
+      }
+      List<Enum<?>> vertexList = vertices[visitV.ordinal()];
+      for (int i = 0; i < vertexList.size(); i++) {
+        Enum<?> vertex = vertexList.get(i);
+        if (!visited[vertex.ordinal()]) {
+          vertexStack.push(vertex);
+        }
+      }
+    }
+
+    return sj.toString();
   }
 
   @Override
   public String breadthFirstSearch(Enum<?> startV) {
-    return null;
+    boolean[] visited = new boolean[vertices.length];
+    StringJoiner sj = new StringJoiner(" ");
+    Queue<Enum<?>> vertexQueue = new LinkedListQueue<>();
+    vertexQueue.enqueue(startV);
+
+    while (!vertexQueue.isEmpty()) {
+      Enum<?> visitV = vertexQueue.dequeue();
+
+      if (visitVertex(visited, visitV)) {
+        sj.add(visitV.toString());
+      }
+
+      List<Enum<?>> vertexList = vertices[visitV.ordinal()];
+      for (int i = 0; i < vertexList.size(); i++) {
+        Enum<?> vertex = vertexList.get(i);
+        if (!visited[vertex.ordinal()]) {
+          vertexQueue.enqueue(vertex);
+        }
+      }
+
+    }
+
+    return sj.toString();
+  }
+
+  private boolean visitVertex(boolean[] visited, Enum<?> vertex) {
+    if (visited[vertex.ordinal()]) {
+      return false;
+    }
+    visited[vertex.ordinal()] = true;
+    return true;
   }
 
   private static class WeightEdge {
